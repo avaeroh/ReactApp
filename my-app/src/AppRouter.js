9ca620ead4a4fdd039aedd380c9b5eaa88053c51
Jon Millington-Hotze <jonathan.mh@hotmail.co.uk>
2019-04-04 14:35:13 +0100

    navbar changes

--- my-app/src/AppRouter.js
+++ my-app/src/AppRouter.js
@@ -23,7 +23,7 @@ export function AppRouter() {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about/">About</Link>
+              <Link to="/about">About</Link>
             </li>
             <li>
               <Link to="/users/">Users</Link>
@@ -32,7 +32,7 @@ export function AppRouter() {
         </nav>
 
         <Route path="/" exact component={Index} />
-        <Route path="/about/" component={About} />
+        <Route path="/about" component={About} />
         <Route path="/users/" component={Users} />
       </div>
     </Router>
